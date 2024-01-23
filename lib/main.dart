b6ded7c371ef43de43fd6aeff6fc83ae569dd4d2
Randa Sabry <randasabry2002@gmail.com>
2024-01-23 20:45:26 +0200

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MaterialApp(home: Scaffold(),));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions( apiKey: "AIzaSyAUDvl3fodKFTPdAJFca8V60UCgZuD4teo",
+          authDomain: "recipesflutterapp-fbc53.firebaseapp.com",
+          projectId: "recipesflutterapp-fbc53",
+          storageBucket: "recipesflutterapp-fbc53.appspot.com",
+          messagingSenderId: "128127910877",
+          appId: "1:128127910877:web:05f8293bd0c4e25c552c34",
+          measurementId: "G-MB78XHYGVW")
+
+  );
+  runApp(MaterialApp(home: Scaffold(backgroundColor: Colors.green,),));
 }
