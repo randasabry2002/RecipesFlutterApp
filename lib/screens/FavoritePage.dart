09cf197ab463a6143889215dd6b68f72e601ffe2
Shimaa-Taha <shimaataha637@gmail.com>
2024-01-24 10:06:12 -0800

    design

---
+++ lib/screens/FavoritePage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'home page.dart';
+
+
+class FavoritePage extends StatefulWidget {
+  final List<Recipe> favoriteRecipes;
+
+  const FavoritePage({Key? key, required this.favoriteRecipes}) : super(key: key);
+
+  @override
+  _FavoritePageState createState() => _FavoritePageState();
+}
+
+class _FavoritePageState extends State<FavoritePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorite Page'),
+      ),
+      body: Container(color: Colors.white,),
+    );
+  }
+}
